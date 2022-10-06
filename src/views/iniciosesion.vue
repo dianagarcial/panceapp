6ddd725be914b3b1cdefbe5898a77c7a01ab7bd4
davidsc1509 <david.sanchez_col@uao.edu.co>
2022-10-06 16:10:46 -0500

    implementacion login

--- src/views/iniciosesion.vue
+++ src/views/iniciosesion.vue
@@ -2,30 +2,55 @@
     <div id="contenido">
         <h1>Ingreso</h1>
         <h2>Que bueno verte de nuevo</h2>
-        <h3>Correo electronico</h3>
-        <input type="email" id="email" name="email" />
-        <h3>Contraseña</h3>
-        <input type="password" />
-        <div id="izq">
-            <button id="retro" @click="ingreso">Ingresar</button>
-            <button id="ir" @click="registro">Registrarse</button>
+        <form @submit.prevent="Login()">
+            <h3>Correo electronico</h3>
+            <input type="email" id="email" name="email" />
+            <h3>Contraseña</h3>
+            <input type="password" id="pass" name="pass" />
+            <div id="izq">
+                <button id="retro" type="submit">Ingresar</button>
+                <button id="ir" @click="registro">Registrarse</button>
 
-        </div>
+            </div>
+
+        </form>
     </div>
 
 
 </template>
 <script>
 
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 
 export default {
+
+
     name: 'Iniciosesion',
+    data() {
+        return {
+            email: '',
+            password: '',
+            error: null,
+            store: useStore(),
+            router: useRouter(),
+        }
+    },
     methods: {
-        ingreso() {
-            this.$router.push('/admin')
+        async Login() {
+            try {
+                await this.store.dispatch('user/logIn', {
+                    email: this.email,
+                    password: this.password
+                })
+                this.router.push('/registro')
+            }
+            catch (err) {
+                this.error=err.message
+            }
         },
         registro() {
-            this.$router.push('/registro')
+            this.router.push('/registro')
         }
 
     }
