91ba627553b16e5895c3d00eab8877f0c7075c24
Diana Marcela Garcia Lopez <diana_ma.garcia@uao.edu.co>
2022-11-21 13:49:06 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import router from './router'
 import store from './store'
 import axios from "axios";
 import VueAxios from 'vue-axios';
-axios.defaults.baseURL='http://localhost:4000/api'
-//axios.defaults.baseURL='https://travelappuao.herokuapp.com/api'
+//axios.defaults.baseURL='http://localhost:4000/api'
+axios.defaults.baseURL='https://travelappuao.herokuapp.com/api'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import VueGoogleMaps from '@fawmi/vue-google-maps'
